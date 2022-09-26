0f89492bb78f59bd6ee1e1f9ed83f55851e96a8e
Arynz&Clem <68093214+ArynzClem@users.noreply.github.com>
2022-09-26 19:18:33 +0700

    Add files via upload

---
+++ TOOLSV1.sh
@@ -0,0 +1,247 @@
+#!/bin/bash
+#version 1.0
+
+#MAU RECODE YA ANJING
+
+
+#01/08/18
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet ArynzClem  | lolcat
+
+echo -b "_____________________________________________________________" | lolcat
+echo -b "TYPE      : TOOLS SERBAGUNA " |lolcat
+echo -b "VERSION   : V.1 " | lolcat
+echo -b "TOTALS    : 15 TOOLS " | lolcat
+echo -b "AUTHOR    : Arynz&Clem$green " |lolcat
+echo -b "PRODUCT BY:MCA:MUSLIM CYBER ARMY 2018 " |lolcat
+echo -b "_____________________________________________________________" | lolcat
+
+sleep 1
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUA " |lolcat
+echo -b $green"[#]> Thanks For Using My Tools" |lolcat
+sleep 1
+echo -b $green"[#]> Arynz&ClemOn Your System" |lolcat
+
+echo -b $green"[#]> See you Again SCRIPTKIDDIESðŸ˜Ž :)..." |lolcat
+figlet  arynz&clem | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""
+echo ""
+
+echo -e "######################################" | lolcat
+echo -e "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
+echo -e "######################################" | lolcat
+
+echo -e  
+echo -e
+
+echo -e "######################################" | lolcat
+echo -e "#GUNAKAN DENGAN BIJAK#" | lolcat
+echo -e "######################################" | lolcat
+
+echo ""
+echo -e "============================" | lolcat
+echo -e $b "1. Install Qlizz${enda}";
+echo -e "============================" | lolcat
+echo -e $b "2. Install CAM-HACK${endcla}";
+echo -e "============================" | lolcat
+echo -e $b "3. Install WEB PHISING${enda}";
+echo -e "============================" | lolcat
+echo -e $b "4. Install SPAM BRUTAL${enda}";
+echo -e "============================" | lolcat
+echo -e $b "5. Install HACK FB${enda}";
+echo -e "============================" | lolcat
+echo -e $b "6. Install SQLMAP${enda}";
+echo -e "============================" | lolcat
+echo -e $b "7. Install Brutex(HACK AKUN SOSIAL MEDIA) ${enda}";
+echo -e "============================" | lolcat
+echo -e $b "8. Install IPGEOLACATION${enda}";
+echo -e "============================" | lolcat
+echo -e $b "9. Install vbug${enda}";
+echo -e "============================" | lolcat
+echo -e $b "10. Install Firecrack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "11. Install XERXES${enda}";
+echo -e "============================" | lolcat
+echo -e $b "12. Install LITESPAM${enda}";
+echo -e "============================" | lolcat
+echo -e $b "13. Install Android-Malware${enda}";
+echo -e "============================" | lolcat
+echo -e $b "14. Install SigPloit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "15. Install TOOLSV3${enda}";
+echo -e "============================" | lolcat
+echo -e $b "00. Exit${enda}";
+echo -e "============================" | lolcat
+echo -e "€[PILIH NOMORNYA]"
+read -p "€root@./Arnyz&Clem=" pil;
+
+# Nmap
+
+case $pil in
+1) git clone https://github.com/RozhakXD/Qlizz
+echo -e "${y}TUTORIAL ADA DI https://github.com/RozhakXD/Qlizz"
+
+;;
+
+#Install  
+
+2) git clone https://github.com/XPH4N70M/HACK-CAMERA.git
+echo -e "${y}TUTORIAL DI https://youtu.be/zaBU9I_KGXk"
+
+;;
+
+#Install 
+
+3) git clone https://github.com/htr-tech/zphisher.git
+echo -e "${y}TUTORIAL DI https://github.com/htr-tech/zphisher.git"
+
+;;
+
+#Install 
+
+4) git clone https://github.com/h20-studio/spambrutalv5.2
+echo -e "${y}TUTORIAL ADA DI https://github.com/h20-studio/spambrutalv5.2"
+
+;;
+
+#Install 
+
+5) git clone https://github.com/Mr-XsZ1/fb
+echo -e "${y}TUTORIAL ADA DI https://github.com/Mr-XsZ1/fb"
+
+;;
+
+#Install 
+
+6) git clone git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+echo -e "${y}TUTORIAL ADA DI https://github.com/sqlmapproject/sqlmap"
+
+;;
+
+#Install D-TECT
+
+7) git clone https://github.com/MrHacker-X/BruteX.git
+echo -e "${y}TUTORIAL ADA DI https://github.com/MrHacker-X/BruteX.git"
+
+;;
+
+#Install 
+
+8) git clone https://github.com/maldevel/IPGeoLocation
+echo -e "${y}TUTORIAL ADA DI https://github.com/maldevel/IPGeoLocation"
+
+;;
+
+#Install 
+
+9) git clone https://github.com/Gameye98/vbug
+echo -e"${y}TUTORIAL ADA DI https://github.com/Gameye98/vbug"
+
+;;
+
+#Install MultiBruteForce(MBF)
+
+10) git clone https://github.com/Ranginang67/Firecrack
+echo -e "${y}TUTORIAL ADA DI https://github.com/Ranginang67/Firecrack"
+
+;;
+
+#Install XERXES
+
+11) git clone https://github.com/zanyarjamal/xerxes
+echo -e "${y} installer xerxer..."
+echo -e "${y} apt install clang"
+echo -e "${y} cd xerxes"
+echo -e "${y} clang xerxes.c -o xerxes"
+echo -e "${y} ./xerxes nama website target 80"
+
+
+;;
+
+#Install LITESPAM
+
+12) git clone https://github.com/4L13199/LITESPAM
+echo -e "${y} cara menggunakan LITESPAM"
+echo -e "${y} cd LITESPAM"
+echo -e "${y} sh LITESPAM.sh"
+
+
+;;
+
+#Install BUAT VIRUS MEMATIKAN
+
+
+13) git clone https://github.com/ashishb/android-malware
+echo -e "${y} Cara membuat virus mematikan"
+echo -e "${y} cd android-malware"
+echo -e "${y} ls"
+echo -e "${y} Nah kalian tinggal masuk ke dictiory virusnya"
+echo -e "${y} Contoh = cd xbot"
+echo -e "${y} ls"
+echo -e "${y} Nah kalian tinggal pindahin virus itu ke sdcard"
+
+
+;;
+
+#Install SigPloit
+
+
+14) git clone https://github.com/SigPloiter/SigPloit
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Sn1per
+
+15) git clone https://github.com/ArynzClem/TOOSLV3
+echo -e "${y} SUDAH TERINSTALL.."
+
+;;
+
+00) echo "AUTHOR: Arynz&Clem" | lolcat
+echo "SEMUA TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUA" | lolcat
+figlet ArynzClem | lolcat
+exit
+;;
+
+*) echo "Sorry, Your choices it's not already [T4T]"
+esac
+done
+done
+
+
+
